add80911f5ee295f7404ee11cb4fd4c896a1f7a3
Alanhassan99 <alanhassan99@yahoo.com>
2026-06-04 16:18:25 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+const Statistics = (props) => {
+  if (props.average) {
+    return (
+      <div>
+        <p>average {props.average}</p>
+        <p>positive {props.positive}%</p>
+      </div>
+    ) ?? <div><p>NOT</p></div>
+  } return (
+    <div><p>No feedback given</p></div>
+  )
+
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + bad + neutral}</p>
+      <Statistics average={(good - bad) / (good + neutral + bad)} positive={good / (good + neutral + bad)} />
+    </div>
+  )
+}
+
+export default App
